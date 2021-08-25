ff0390b00ce0dc0ac5973e08942c1179fdde1e04
迷你强 <chenglei1234@gmail.com>
2021-08-25 15:23:14 +0800

    测试代码更新

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,33 +1,32 @@
-//#include <Arduino.h>
 #include "v9260f.h"
+
 V9260F v9260f;
+HardwareSerial Serial3(PC11, PC10);   // 通讯调试串口
+uint8_t Device_addr = 0; //设备地址
 void setup()
 { 
-  digitalWrite(0,LOW);
-  delay(200);
-  digitalWrite(0,HIGH);
-  //delay(2000);
+
   SerialUSB.begin(115200);
 
-  Serial1.begin(2400, SERIAL_8O1);
-  v9260f.begin(Serial1);
+  Serial3.begin(2400, SERIAL_8O1);
+  v9260f.begin(Serial3);
   
   //计量控制寄存器
-  v9260f.writeRegisterData(0x0183, 0x0E000000, 0);
-  //v9260f.writeRegisterData(0x0184, 0x430000, 0);    //0a0b0900
-  v9260f.writeRegisterData(0x0184, 0x0a0b0900, 0);  //有效
+  v9260f.writeRegisterData(0x0183, 0x0E000000, Device_addr);
+  //v9260f.writeRegisterData(0x0184, 0x430000, Device_addr);    //0a0b0900
+  v9260f.writeRegisterData(0x0184, 0x0a0b0900, Device_addr);  //有效(神秘参数)
   
   //模拟控制寄存器
   //v9260f.writeRegisterData(0x0185, 0x3d200440, 0);  //默认参数
-  v9260f.writeRegisterData(0x0185, 0xDF000440, 0);
-  v9260f.writeRegisterData(0x0186, 0x30000000, 0);
+  v9260f.writeRegisterData(0x0185, 0xDF000440, Device_addr);
+  v9260f.writeRegisterData(0x0186, 0x30000000, Device_addr);
   //v9260f.writeRegisterData(0x0187, 0x00000000, 0);
-  v9260f.writeRegisterData(0x0187, 0x0000000F, 0);
+  v9260f.writeRegisterData(0x0187, 0x0000000F, Device_addr);
   //系统配置寄存器
-  v9260f.writeRegisterData(0x0180, 0x01FB0004, 0);
+  v9260f.writeRegisterData(0x0180, 0x01FB0004, Device_addr);
   
   //带通滤波器
-  v9260f.writeRegisterData(0x0125, 0x811D2BA7, 0);
+  v9260f.writeRegisterData(0x0125, 0x811D2BA7, Device_addr);
   
   //
   //writeRegisterData(0x012c, 0xFBE93380, 0);
@@ -36,7 +35,7 @@ void setup()
   //uint32_t sum_check = 0xFFFFFFFF - (0x3d200440 + 0x30000000 + 0x430000 + 0x811D2BA7);
   uint32_t sum_check = 0xFFFFFFFF - (0x0E000000 + 0x0a0b0900 + 0xDF000440 + 0x30000000 + 0x01FB0004 + 0x811D2BA7);
   //SerialUSB.println(sum_check,HEX);
-  v9260f.writeRegisterData(0x0133, sum_check, 0);
+  v9260f.writeRegisterData(0x0133, sum_check, Device_addr);
   delay(100);
 }
 
@@ -44,13 +43,14 @@ void loop()
 {
   //byte data[4];
   uint16_t _addr = 0x11C;
-  uint32_t data = v9260f.readRegisterData(_addr ,0);
+  
+  int32_t Data = v9260f.readRegisterData(_addr ,Device_addr);
 
   SerialUSB.print(_addr,HEX);
   SerialUSB.print(":");
-  SerialUSB.print(data);
+  SerialUSB.print(Data);
   SerialUSB.print(",");
-  SerialUSB.println(data / 103452);
+  SerialUSB.println(Data / 103452);
   /*
   SerialUSB.print(data[3],HEX);
   SerialUSB.print(",");
--- v9260f.h
+++ v9260f.h
@@ -7,7 +7,7 @@ class V9260F
 {
     public:
         void begin(HardwareSerial& SerialData);   //初始化实例化串口
-        void systemConfig();
+        void systemConfig();  
         void analogConfig0();
         void analogConfig1();
         void measureConfig0();
@@ -16,15 +16,13 @@ class V9260F
 
 
         void writeRegisterData(uint16_t Addr, uint32_t writeData, byte Device_addr);   //写入数据至寄存器
-        uint32_t readRegisterData(uint16_t Addr, byte Device_addr);
-        void readRegisterData(uint16_t Addr, byte Device_addr,byte *DataOutput);
-        
-
+        uint32_t readRegisterData(uint16_t Addr, byte Device_addr);                 //从寄存器获取数据 uint32
+        void readRegisterData(uint16_t Addr, byte Device_addr,byte *DataOutput);     //4字节数组传出
+   
     private:
         HardwareSerial *_Serial;
         uint32_t Register[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         byte uchar_checksum(byte *data, byte dataSize);   //专用和校验计算
-
 };
 
 
